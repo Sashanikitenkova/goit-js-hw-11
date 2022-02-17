9316504a1adb33cd01581347386e56403fd2a978
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-02-17 15:39:14 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 
+const searchForm = document.querySelector(".search-form");
+const galleryEl = document.querySelector(".gallery");
+
+searchForm.addEventListener('submit, onSearch');
+
+function onSearch (e) {
+    e.preventDefault();
+
+    const url = 'https://pixabay.com/api/?key=25749295-c1c91c3a002bacdc6232fef3b&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1';
+
+    fetch(url)
+    .then(r => r.json())
+    .then(console.log);
+
+};
+
+
+
+
+
+const url = 'https://pixabay.com/api/?key=25749295-c1c91c3a002bacdc6232fef3b&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1';
+
+fetch(url)
+.then(r => r.json())
+.then(console.log);
 
 
 
